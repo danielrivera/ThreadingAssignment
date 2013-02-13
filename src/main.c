0d1f2e9fd7d69adb0e66ba209d3ebd24965b7f35
Daniel Rivera <daniel@macadamian.com>
2013-02-12 20:56:40 -0500

    Added exercise code

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@
 #include <bps/screen.h>
 #include <fcntl.h>
 #include <screen/screen.h>
+#include <pthread.h>
 
 static bool shutdown;
 
@@ -83,9 +84,55 @@ handle_event()
     }
 }
 
-int
-main(int argc, char **argv)
+//TODO Define global data structures to be used
+
+
+/**
+ * This thread is responsible for pulling data off of the shared data
+ * area and processing it using the process_data() API.
+ */
+void *reader_thread(void *arg) {
+    //TODO: Define set-up required
+
+    while(1) {
+        //TODO: Define data extraction (queue) and processing
+    }
+
+    return NULL;
+}
+
+
+/**
+ * This thread is responsible for pulling data from a device using the
+ * get_external_data() API and placing it into a shared area for later
+ * processing by one of the reader threads.
+ */
+void *writer_thread(void *arg) {
+    //TODO: Define set-up required
+
+    while(1) {
+        //TODO: Define data extraction (device) and storage
+    }
+
+    return NULL;
+}
+
+
+#define M 10
+#define N 20
+
+int main(int argc, char **argv)
 {
+    int i;
+
+    for(i = 0; i < N; i++) {
+        pthread_create(NULL, NULL, reader_thread, NULL);
+    }
+
+    for(i = 0; i < M; i++) {
+        pthread_create(NULL, NULL, writer_thread, NULL);
+    }
+
     const int usage = SCREEN_USAGE_NATIVE;
 
     screen_context_t screen_ctx;
